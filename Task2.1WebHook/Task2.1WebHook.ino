a70a4a3cc599201a2ccdc83c31e2e06292cc397e
ivyztech <108420656+ivyztech@users.noreply.github.com>
2024-03-18 20:19:22 +1100

    Add files via upload

---
+++ Task2.1WebHook/Task2.1WebHook.ino
@@ -0,0 +1,111 @@
+#include <WiFiNINA.h>
+#include "secrets.h"
+#include "ThingSpeak.h" 
+
+#define DHTPIN 2
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+// Initialize our values
+int number1 = 0;
+int number2 = random(0,100);
+int number3 = random(0,100);
+int number4 = random(0,100);
+String myStatus = "";
+
+void setup() {
+  Serial.begin(115200);      // Initialize serial 
+  while (!Serial) {
+    Serial.println(F("DHTxx test!")); // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  dht.begin();
+  ThingSpeak.begin(client);  // Initialize ThingSpeak 
+}
+
+void loop() {
+
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   float f = dht.readTemperature(true);
+
+   // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  // set the fields with the values
+  ThingSpeak.setField(1, t);
+  ThingSpeak.setField(2, h);
+
+  // figure out the status message
+  if(number1 > number2){
+    myStatus = String("field1 is greater than field2"); 
+  }
+  else if(number1 < number2){
+    myStatus = String("field1 is less than field2");
+  }
+  else{
+    myStatus = String("field1 equals field2");
+  }
+  
+  // set the status
+  ThingSpeak.setStatus(myStatus);
+  
+  // write to the ThingSpeak channel 
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  // change the values
+  number1++;
+  if(number1 > 99){
+    number1 = 0;
+  }
+  number2 = random(0,100);
+  number3 = random(0,100);
+  number4 = random(0,100);
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
---
+++ Task2.1WebHook/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Nikki"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "nikki202011"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2467074			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "UR2XHZO8ZXWNDSR1"   // replace XYZ with your channel write API Key
